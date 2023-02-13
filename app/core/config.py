352a95ba9ad60177a66e3f803e9f2b29348d54e3
Sergey Patrushev <PatrushevSergeyVal@yandex.ru>
2023-02-13 11:24:54 +0300

    structure, config(env), db

---
+++ app/core/config.py
@@ -0,0 +1,23 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    """
+    Defines the basic settings of the application: application name,
+    description, database connection and secret key. These settings
+    are by default. Initially, these settings are checked in the
+    operation system environment variables. It is also possible
+    to directly access the .env file. 
+    """
+    app_title: str = 'Cat charity fund'
+    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
+    description: str = '## **API for the cat charity foundation**'
+    secret: str = 'SECRET'
+
+    class Config:
+        """
+        Checks variables in the .env file.
+        """
+        env_file = '.env'
+
+settings = Settings()
